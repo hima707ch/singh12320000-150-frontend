a02e7f5026d0ef6f821071dacd85d235b77621f6
root <root@0b76f6d39c7d>
2025-07-05 19:30:19 +0000

    Frontend setup and components update

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Body from './Body';
+import images from '../assets/images';
+
+const HomePage = () => {
+  return (
+    <div id="HomePage_1">
+      <Body />
+    </div>
+  );
+};
+
+export default HomePage;
+
+"features": [
+    {"id": "1", "name": "Feature 1", "description": "Description for feature 1."},
+    {"id": "2", "name": "Feature 2", "description": "Description for feature 2."},
+    {"id": "3", "name": "Feature 3", "description": "Description for feature 3."}
+ ]
+
+
